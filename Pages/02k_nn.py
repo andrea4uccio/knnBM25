7ac59471e3081d17f196ff154b89a8c339c57729
Andrea <47042838+andrea4uccio@users.noreply.github.com>
2025-03-06 17:07:02 +0100

    Sistemate knn e Conclusioni

--- Pages/02k_nn.py
+++ Pages/02k_nn.py
@@ -11,50 +11,50 @@ st.set_page_config(
 
 
 
-st.title("Come funziona k-nn ")
-st.write("""
+st.markdown("## Come funziona k-nn ")
+st.markdown("""
 Speighiamo in breve il concetto dietro il funzionamento di  k-nn.
 				 
-L'algoritmo di classificazione k-NN (k-nearest neighbors) è un metodo di apprendimento supervisionato che assegna una classe a un campione in base alle classi dei suoi k vicini più prossimi nel dataset di addestramento. Funziona in questo modo:
+L'algoritmo di classificazione k-NN (k-nearest neighbors) \u00E8 un metodo di apprendimento supervisionato che assegna una classe a un campione in base alle classi dei suoi k vicini pi\u00F9 prossimi nel dataset di addestramento. Funziona in questo modo:
 
 1. Calcolo della distanza: Per ogni nuovo dato che deve essere classificato, l'algoritmo calcola la distanza tra il punto da classificare e tutti i punti nel dataset di addestramento (di solito usando la distanza euclidea).
 
-2. Identificazione dei vicini: Si selezionano i k punti più vicini al punto da classificare, dove k è un parametro scelto dall'utente.
+2. Identificazione dei vicini: Si selezionano i k punti pi\u00F9 vicini al punto da classificare, dove k \u00E8 un parametro scelto dall'utente.
 
-3. Voto di maggioranza: La classe assegnata al punto da classificare è la classe più frequente tra i k vicini selezionati.
+3. Voto di maggioranza: La classe assegnata al punto da classificare \u00E8 la classe pi\u00F9 frequente tra i k vicini selezionati.
 
 Scelta di k: Il valore di k influisce sul risultato: valori troppo piccoli possono essere sensibili al rumore, mentre valori troppo grandi possono generalizzare troppo e perdere dettagli importanti.
 
-QUesto sistema sara` la base per ottenere dai risultati della query iniziale dei descrittori aggiuntivi per fare QE.
+QUesto sistema sar\u00E0 la base per ottenere dai risultati della query iniziale dei descrittori aggiuntivi per fare QE.
 """)
 
-st.title("k-nn e QE")
-st.write("""
-L'approccio che stiamo usando e` simile alla pseudo-relevance feedback. Nella pseudo-relevance feedback si assume che i documenti in posizione piu alta come rilevanti.
+st.markdown(" ## k-nn e QE")
+st.markdown("""
+L'approccio che stiamo usando \u00E8 simile alla pseudo-relevance feedback. Nella pseudo-relevance feedback si assume che i documenti in posizione piu alta come rilevanti.
 L'idea quindi cambia, usiamo un cluster di documenti per trovare documenti dominanti per il set reperito iniziale e ripetutamente fornire i documenti per enfatizzare gli argomenti principali di una query. 
 """)
 
-st.title("Problema")
-st.write("""
-				 I documenti reperiti in posizione alta contengono rumore, se la P@10 e` 0.5, di 10 documenti, 5 sono non rilevanti.
-				 Il che puo provocare un drift dei risultati delle query che invece di avvicinarsi a documenti rilevanti si puo avvicinare a quelli non rilevanti.
-				 Trovare il cluster ottimale e difficile, quindi useremo una serie di gruppi rilevanti per il feedback permettendo cluster sovrapponibili per i top-retrieved e alimentando ripeturamente i documenti dominantiche appaiono in piu cluster di alto rango ci aspettiamo che QE possa portare a risultati piu precisi.
-				 la motivazione dell'utilizzo di cluster deriva dal fatto che i documenti inposizione piu` alta hanno un ordinamento "query-oriented" che nono considera la relazione tra documenti.
-				 Il problema post in precedenza e` come scegliere i termini di espansione, che devono essere vicini alla quey. Selezionando ripetutamente i documenti dominanti risolviamo questa difficolta.
-				 """)
-
-st.title("Funzionamento")
-st.write("""
-*** Documento dominante *** : Assumiamo un documento dominanto per una query quel documento con un ottima rappresentazione dei topics di una qury, ovvero uno con diversi vicini con alta similarita`. Nel nostro caso di cluster sovrapposti un documento dominante sara` quello che appare in molteplici cluster con rango elevato. Dal documento dominante rilevato possiamo ricavare i temmini di espansioneche reperirira documenti rletivi a tutti i sottitopics.
-		1. I documenti vengon o riperiti data una query usando l'API search() di elasticsearch che di base usa il BM25 senza QE
-		2. Vengono generati dei cluster di k-nearest vicini per i primi N docuimenti reperiti pr trovare documenti dominanti. 
-		3. In k-NN ogni docunmento hja un ruolo centrale nel creare il proprio cluster con k vicini per similarita.
-		4. Il ranog viene deciso da un language model basato su cluster.
-		5. I termini id espansione vengono selezionati usando il modello di rilevanza per ogni documento nei top-ranked cluster.			 
+st.markdown("## Problema")
+st.markdown("""
+I documenti reperiti in posizione alta contengono rumore, se la P@10 \u00E8 0.5, di 10 documenti, 5 sono non rilevanti.
+Il che puo provocare un drift dei risultati delle query che invece di avvicinarsi a documenti rilevanti si puo avvicinare a quelli non rilevanti.
+Trovare il cluster ottimale \u00e8 difficile, quindi useremo una serie di gruppi rilevanti per il feedback permettendo cluster sovrapponibili per i top-retrieved e alimentando ripeturamente i documenti dominantiche appaiono in piu cluster di alto rango ci aspettiamo che QE possa portare a risultati piu precisi.
+la motivazione dell'utilizzo di cluster deriva dal fatto che i documenti inposizione pi\u00F9 alta hanno un ordinamento "query-oriented" che nono considera la relazione tra documenti.
+Il problema post in precedenza \u00E8 come scegliere i termini di espansione, che devono essere vicini alla quey. Selezionando ripetutamente i documenti dominanti risolviamo questa difficolta.
 """)
 
-st.title("Implementazone")
+st.markdown("## Funzionamento")
+st.markdown("""
+**Documento dominante** : Assumiamo un documento dominanto per una query quel documento con un ottima rappresentazione dei topics di una qury, ovvero uno con diversi vicini con alta similarit\u00E0. Nel nostro caso di cluster sovrapposti un documento dominante sar\\U00E0 quello che appare in molteplici cluster con rango elevato. Dal documento dominante rilevato possiamo ricavare i temmini di espansioneche reperirira documenti rletivi a tutti i sottitopics.
+1. I documenti vengon o riperiti data una query usando l'API search() di elasticsearch che di base usa il BM25 senza QE
+2. Vengono generati dei cluster di k-nearest vicini per i primi N docuimenti reperiti pr trovare documenti dominanti. 
+3. In k-NN ogni docunmento hja un ruolo centrale nel creare il proprio cluster con k vicini per similarit\u00e0.
+4. Il rango viene deciso da un language model basato su cluster.
+5. I termini di espansione vengono selezionati usando il modello di rilevanza per ogni documento nei top-ranked cluster.			 
+""")
 
+st.markdown("# Implementazone")
+st.markdown("Di seguito viee riportato il frammento di codice repsonsabile per la QE e l'uso del k-nn. L'implementazione dell'algoritmo di clustering non viene affrontato in qeusto progetto, viene usata l'implementazione presente nel pacchetto __scikit-learn__")
 codice = """
 # Calcola la DTM, fa clustering e crea la query espansa
 # @param server connessione al server ElasticSearch                                   IP
@@ -184,24 +184,28 @@ def QE_kNN(server, INDEXNAME, N_collezione, querytext, first_query_size, k, min_
 """
 st.code(codice, language='python')
 
-st.title("Risultati e parametri")
-st.text("""Ci sono stati diversi parametri da ottimizzare, di seguito si riportano i due piu rilevanti
-La prima configurazione consiste nel Reperire 10 documenti dominanti, selezionare 25 descrittori per QE, impostare il peso dei nuovi descrittore a 0.7
-La seconda usa ancora 10 doicumenti dominanti, prende 100 descrittori e imposta il peso a 0.9
+st.markdown("# Risultati e parametri")
+st.markdown("""Ci sono stati diversi parametri da ottimizzare:
+  - **N**: numero di documenti dominanti;
+  - **e**: numero di descrittori da estrarre e aggiungere alla query iniziale;
+  - **$\\lambda$**: peso da dare ai nuovi descrittori nella query espansa.
 
-	
+Sono riportate le due configurazioni che si sono distinte maggirmente:
+1. Prima configurazione:
+  - **N** = 10;
+  - **e** = 25;
+  - **$\\lambda$** = 0.7.
+2. Seconda configurazione:
+  - **N** = 10;
+  - **e** = 100;
+  - **$\\lambda$** = 0.9.	
 """)
 
-st.text("""Si nota che nel training set la ocnfigurazione 10_25_07 aumenta la map, mentre sempre nel training set il modello con 10_100_09 massimizza la P@5.""")
-st.text("""Non vengono inseriti i risultati del training in quanto i documenti generati da trec-eval sono tanti, Vengono pero inseriti i risultati del test.""")
-
-
-				
-
+st.markdown(""" ## Giustificazione scelta parametri""")
+st.markdown("Mostriamo come variano le metriche scelte per l'analisi, ovvero **map** e **P@5**")
 
 
 bm25 = pl.read_csv("./Data/Eval_Test/test_results_bm25_eval.txt", has_header= False, separator="\t")
-
 kn2507 = pl.read_csv("./Data/Eval_Test/test_results_10_25_07_eval.txt", has_header= False, separator="\t")
 kn10009 = pl.read_csv("./Data/Eval_Test/test_results_10_100_09_eval.txt", has_header= False, separator="\t")
 
@@ -211,54 +215,45 @@ dati09 = kn10009.filter((pl.col("column_1").str.starts_with("map")) | (pl.col("c
 
 datibm[0,0] = "map_BM25"
 datibm[1,0] = "P@5_BM25"
-
-dati07[0,0] = "map_07"
-dati07[1,0] = "P@5_07"
-dati09[0,0] = "map_09"
-dati09[1,0] = "P@5_09"
+dati07[0,0] = "map_first"
+dati07[1,0] = "P@5_first"
+dati09[0,0] = "map_second"
+dati09[1,0] = "P@5_second"
 
 df_unito = pl.concat([datibm, dati07, dati09])
-
-
+df_unito = df_unito.rename({"column_1" : "configurazione"})
 # Creare il grafico per p5~query
-chart_p5 = alt.Chart(df_unito).mark_bar().encode(
-  	x=alt.X('column_1', title = "Configurazione considerata"),  # Impostazione dei limiti per l'asse x
+chart = alt.Chart(df_unito).mark_bar().encode(
+  	x=alt.X('configurazione', title = "Configurazione considerata"),  # Impostazione dei limiti per l'asse x
     y=alt.Y('column_3', title = "valore precisione"),
-		color = 'column_1'		
+		color = 'configurazione'		
 ).properties(
 	title = "Grafico a barre precisione ~ configurazione usata (The higher the better)"
 )
 
 # Visualizzare il grafico
-st.altair_chart(chart_p5, use_container_width= True)
+st.altair_chart(chart, use_container_width= True)
 
-st.text("""
-Notiamo dai grafici quanto riferito in precedenza, la p@5 e maggiore con 09, mentre map e maggior econ 07. 
-				""")
-
-st.text("""
-Notiamo inoltre che entrambe le scelte dei parametri migliorano sensibilmente le metriche di precisione. Quindi potremmo da subito dire che QE con clustering funziona. 
-Prima pero vediamo come si comportano le query singolarmente.
-				""")
+st.markdown("""
+Dai grafici notiamo che i risultati relativi a tutte le query nell'insieme di test migliorano rispetto la configurazione senza QE.
+Possiamo inoltre osservare che la **configurazione 1** migliora il map mentre **configurazione 2** migliora la P@5.
+Prima di dire che il metodo funzioni \u00e8 opportuno fare un analisi per ogni query. 
+""")
 
 
 
-st.title("Modello che massimizza P@5")
+st.markdown("# Modello che massimizza P@5")
 st.markdown("""Prendiamo in analisi il modello che massimizza la metrica P@5, ovvero quello con parametri $lambda = 0.9$, $e = 100$ ed $N$ = 10. 
-
 Valutiamo come le singole query vengono influenzate dall'uso di questo modello e quali migliorano o peggiorano""")
 
 
 querybm25 = pl.read_csv("./Data/Eval_Queries/bm25_evalQ.txt", has_header= True)
 query09 = pl.read_csv("./Data/Eval_Queries/10_100_09_evalQ.txt", has_header= True)
-# Creare il grafico per p5~query
 
 
 selected_x = st.slider("Seleziona il valore di query per p@5", min_value=600, max_value=700, value=(600, 700))
 filtered_bm = querybm25.filter((querybm25['id_Q'] >= selected_x[0]) & (querybm25['id_Q'] <= selected_x[1]))
 filtered_09 = query09.filter((query09['id_Q'] >= selected_x[0]) & (query09['id_Q'] <= selected_x[1]))
-
-
 
 chart_bm = alt.Chart(filtered_bm).mark_point(color = '#FF6347', filled = True).encode(
   	x=alt.X('id_Q', scale=alt.Scale(domain=selected_x)),  # Impostazione dei limiti per l'asse x
@@ -272,36 +267,28 @@ chart_09 = alt.Chart(filtered_09).mark_point(color = '#32CD32', filled = True).e
 		color = "method"
 )
 
-# Visualizzare il grafico
 st.altair_chart(chart_bm+chart_09, use_container_width= True)
 
 st.text("""
-Il grafico non e molto suggestivo, pero possiamo notare che alcune query rimangono invariate, altre invece cambiano sensibilmente.
+Il grafico non \u00e8 molto suggestivo, per\u00f2 possiamo notare che alcune query rimangono invariate, altre invece cambiano sensibilmente.
 """ )
 
 st.markdown(f"""### Prendiamo per esempio le seguenti query:
-- 622: "Price Fixing" con un P@5 senzas QE di {querybm25[21,1]} mentre usando QE otteniamo {query09[21,1]}. Il nostro meteodo ha funzionato egregiamente, anche se questo e` un caso di esempio dove viene massimizzata la differenza.
+- 622: "Price Fixing" con un P@5 senzas QE di {querybm25[21,1]} mentre usando QE otteniamo {query09[21,1]}. Il nostro meteodo ha funzionato egregiamente, anche se questo \u00E8 un caso di esempio dove viene massimizzata la differenza.
 - 626: "Human Stampede" con P@5 senza QE di {querybm25[25,1]}, mentre usando QE P@5 diventa di {query09[25,1]}. Ovvero viene dimezzata la precisione. 
-
 """)
 
 
-
-st.title("Modello che massimizza Map")
+st.markdown("# Modello che massimizza map")
 st.markdown("""Prendiamo in analisi il modello che massimizza la metrica map, ovvero quello con parametri $lambda = 0.7$, $e = 25$ ed $N$ = 10. 
-
 Valutiamo come le singole query vengono influenzate dall'uso di questo modello e quali migliorano o peggiorano""")
 
 
 query07 = pl.read_csv("./Data/Eval_Queries/10_25_07_evalQ.txt", has_header= True)
-# Creare il grafico per p5~query
-
 
 selected_x = st.slider("Seleziona il valore di query per map", min_value=600, max_value=700, value=(600, 700))
 filtered_bm = querybm25.filter((querybm25['id_Q'] >= selected_x[0]) & (querybm25['id_Q'] <= selected_x[1]))
 filtered_07 = query07.filter((query07['id_Q'] >= selected_x[0]) & (query07['id_Q'] <= selected_x[1]))
-
-
 
 chart_bm = alt.Chart(filtered_bm).mark_point(color = '#FF6347', filled = True).encode(
   	x=alt.X('id_Q', scale=alt.Scale(domain=selected_x)),  # Impostazione dei limiti per l'asse x
@@ -315,18 +302,16 @@ chart_07 = alt.Chart(filtered_07).mark_point(color = '#32CD32', filled = True).e
 		color = "method"
 )
 
-# Visualizzare il grafico
 st.altair_chart(chart_bm+chart_07, use_container_width= True)
 
 st.text("""
-Il grafico non e molto suggestivo, pero possiamo notare che alcune query rimangono invariate, altre invece cambiano sensibilmente.
+Il grafico non \u00e8 molto suggestivo, per\u00F2 possiamo notare che alcune query rimangono invariate, altre invece cambiano sensibilmente.
 """ )
 
 st.markdown(f"""### Prendiamo per esempio le seguenti query:
-- 607: "Human genetic code" con un map senzas QE di {querybm25[6,2]} mentre usando QE otteniamo {query09[6,2]}. Il nostro meteodo ha funzionato egregiamente, anche se questo e` un caso di esempio dove viene massimizzata la differenza.
+- 607: "Human genetic code" con un map senzas QE di {querybm25[6,2]} mentre usando QE otteniamo {query09[6,2]}. Il nostro meteodo ha funzionato egregiamente, anche se questo \u00E8 un caso di esempio dove viene massimizzata la differenza.
 - 626: "Human Stampede" con map senza QE di {querybm25[25,2]}, mentre usando QE mapdiventa di {query09[25,2]}. meno della meta della precisione. 
-
 """)
 
 
-st.markdown("""Si possono fare ulteriori considerazioni sul perche' alcune query hanno funzionato meglio di altre ma non e` l'obbiettivo di questo progetto.""")
+st.markdown("""Si possono fare ulteriori considerazioni sul perch\u00e9 alcune query hanno funzionato meglio di altre ma non \u00E8 l'obbiettivo di questo progetto.""")
--- Pages/03conclusioni.py
+++ Pages/03conclusioni.py
@@ -7,16 +7,16 @@ st.markdown("# Conclusione")
 
 st.markdown("""
 Abbiamo visto come ci sia una certa differenza tra le metodologie usate. 
-Inoltre, tornando alla prima anali, quella che considera globalmente $map$ e $P@5$ QE sembra migliorare le metriche. 
+Inoltre, tornando alla prima analisi, quella che considera globalmente **map** e **P@5** QE sembra migliorare le metriche. 
 Ma le differenze sono significative?            
 """)
 
 
-st.markdown("""## Test per significativita`""")
+st.markdown("""## Test per significativit\u00e0""")
 st. markdown("""I test che useremo sono *Paired t-test* e *Wilcoxon signed-rank test*. Entrambi servono a confrontare due gruppi di dati correlati ma si basano su assunti diversi.
-             - Paired t-test e` un test che assume normlita` dei dati;
-             - Wilcoxon non fa assunzione parametriche sulla distribuzione dei dati
-             Li useremo entrambi e proveremo a trarre delle conclusioni
+- Paired t-test \u00e8 un test che assume normalit\u00e0 dei dati;
+- Wilcoxon non fa assunzione parametriche sulla distribuzione dei dati
+Li useremo entrambi e proveremo a trarre delle conclusioni
 """)
 
 
@@ -24,23 +24,32 @@ bm25 = pl.read_csv("./Data/Eval_Queries/bm25_evalQ.txt", has_header= True)
 p5 = pl.read_csv("./Data/Eval_Queries/10_100_09_evalQ.txt", has_header= True)
 map = pl.read_csv("./Data/Eval_Queries/10_25_07_evalQ.txt", has_header= True)
 
-st.markdown("## Analisi $map$")
-print(bm25)
-st.markdown(f"""Il risultato del test Paired t-test e {round(sci.stats.ttest_rel(bm25[:,2], p5[:,2])[1],2)}, che a un livello di significativita` $alpha$ = 0.05, viene accettata ipotesi nulla ovvero le medie non shanno differenza""")
-st.markdown(f"""Il risultato del test Paired t-test e {round(sci.stats.ttest_rel(bm25[:,2], map[:,2])[1],2)}, che a un livello di significativita` $alpha$ = 0.05, viene accettata ipotesi nulla ovvero le medie non shanno differenza""")
+st.markdown("## Analisi map")
 
-st.markdown("## Analisi $p@5$")
-print(bm25)
-st.markdown(f"""Il risultato del test Paired t-test e {round(sci.stats.ttest_rel(bm25[:,1], p5[:,1])[1],2)}, che a un livello di significativita` $alpha$ = 0.05, viene accettata ipotesi nulla ovvero le medie non shanno differenza""")
-st.markdown(f"""Il risultato del test Paired t-test e {round(sci.stats.ttest_rel(bm25[:,1], map[:,1])[1],2)}, che a un livello di significativita` $alpha$ = 0.05, viene accettata ipotesi nulla ovvero le medie non shanno differenza""")
+st.markdown("#### Paired t-test")
+st.markdown(f"""Il risultato del test Paired t-test \u00e8 {round(sci.stats.ttest_rel(bm25[:,2], p5[:,2])[1],2)}, che a un livello di significativit\u00e0 $alpha$ = 0.05, viene accettata ipotesi nulla ovvero le medie non shanno differenza""")
+st.markdown(f"""Il risultato del test Paired t-test \u00e8 {round(sci.stats.ttest_rel(bm25[:,2], map[:,2])[1],2)}, che a un livello di significativit\u00e0 $alpha$ = 0.05, viene accettata ipotesi nulla ovvero le medie non shanno differenza""")
+
+st.markdown("#### Wilcoxon")
+st.markdown(f"""Il risultato del test Wilcoxon \u00e8 {round(sci.stats.wilcoxon(bm25[:,2], p5[:,2])[1],2)}, che a un livello di significativit\u00e0 $alpha$ = 0.05, viene accettata ipotesi nulla ovvero le medie non shanno differenza""")
+st.markdown(f"""Il risultato del test Wilcoxon \u00e8 {round(sci.stats.wilcoxon(bm25[:,2], map[:,2])[1],2)}, che a un livello di significativit\u00e0 $alpha$ = 0.05, viene accettata ipotesi nulla ovvero le medie non shanno differenza""")
+
+
+st.markdown("## Analisi P@5")
+st.markdown(f"""Il risultato del test Paired t-test \u00e8 {round(sci.stats.ttest_rel(bm25[:,1], p5[:,1])[1],2)}, che a un livello di significativit\u00e0 $alpha$ = 0.05, viene accettata ipotesi nulla ovvero le medie non shanno differenza""")
+st.markdown(f"""Il risultato del test Paired t-test \u00e8 {round(sci.stats.ttest_rel(bm25[:,1], map[:,1])[1],2)}, che a un livello di significativit\u00e0 $alpha$ = 0.05, viene accettata ipotesi nulla ovvero le medie non shanno differenza""")
+
+st.markdown("#### Wilcoxon")
+st.markdown(f"""Il risultato del test Wilcoxon \u00e8 {round(sci.stats.wilcoxon(bm25[:,1], p5[:,1])[1],2)}, che a un livello di significativit\u00e0 $alpha$ = 0.05, viene accettata ipotesi nulla ovvero le medie non shanno differenza""")
+st.markdown(f"""Il risultato del test Wilcoxon \u00e8 {round(sci.stats.wilcoxon(bm25[:,1], map[:,1])[1],2)}, che a un livello di significativit\u00e0 $alpha$ = 0.05, viene accettata ipotesi nulla ovvero le medie non shanno differenza""")
+
 
 st.markdown("""## Conclusione""")
-st.markdown("""QUindi possiamo dire che l'unico valore significativoe e quello che confronta le map tra modello con QE che massimizza il map e il modello senza QE.""")
+st.markdown("""Quindi possiamo dire che l'unico valore significativo \u00e8 quello che confronta le map tra modello con QE che massimizza il map \u00e8 il modello senza QE.""")
 
 st.markdown("""Potremmo concludere che dicendo che QE con k-nn migliora la precisione, almeno la mean average precisio, per il reperimento di documenti rilevanti. 
-            Se consideriamo come unica e sola metrica map, e` un metodo da scegliere, ma;
-            - per il training si e ` semplicemente diviso il dataset in prime 150 come training e ultime 100 come test. Avendo quindi un dataset limitato non e` detto che sia la soluzione migliore. Implementare la cross validation sarebbe preferibile 
-            - sempre per la fase di training e vista la natura del problema non e` stato possibile analizzare ogni combinazione possibile per i parametri $lambda$, $e$,ed $N$, e` quinid probabile che non siano stati considerati parametri migliori
-            - i tempi di esecuzione non sono spaventosamente lunghi ma le operazioni che fa il calcolatore aumenta, aumentandone cosi l'inefficienza.
+Se consideriamo come unica e sola metrica map, \u00e8 un metodo da scegliere, ma;
+- per il training si \u00e8 semplicemente diviso il dataset in prime 150 come training e ultime 100 come test. Avendo quindi un dataset limitato non \u00e8 detto che sia la soluzione migliore. Implementare la cross validation sarebbe preferibile; 
+- sempre per la fase di training e vista la natura del problema non \u00e8 stato possibile analizzare ogni combinazione possibile per i parametri $lambda$, $e$,ed $N$, \u00e8 quinid probabile che non siano stati considerati parametri migliori;
+- i tempi di esecuzione non sono spaventosamente lunghi ma le operazioni che fa il calcolatore aumenta, aumentandone cosi l'inefficienza.
 Questi sono problemi che possono essere risolti con del tempo a disposizione per il training del modello e magari considerare alcuni parametri per esempio $lambda$ fuori dall'insieme da considerare e trattarlo invece come parametro scelto dal programmatore e tenuto fisso per ogni configuarzione.""")
-
